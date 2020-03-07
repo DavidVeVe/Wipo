36e6dcbf032ec261beb2b3b852c02eb4ae26130c
Armando Velazquez <davidarmando.gdl@live.com>
2020-03-07 02:51:00 -0600

    media queries 630px

--- slides/index.js
+++ slides/index.js
@@ -1,3 +1,14 @@
+const burguer = document.getElementById("burguer");
+const menu = document.getElementById("menu");
+
+burguer.addEventListener("click", () => {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+  } else {
+    menu.classList.add("active");
+  }
+});
+
 new Glide(".glide", {
   perTouch: 1,
   keyboard: true,
@@ -9,3 +20,5 @@ new Glide(".second__slide", {
   gap: 40,
   startAt: 0
 }).mount();
+
+// NAVBAR CONTROLLER BELOW
